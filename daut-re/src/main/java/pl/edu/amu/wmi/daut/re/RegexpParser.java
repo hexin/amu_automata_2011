6af179899c3fbb45d96a1ccd5e5c5e0bf34fb0c8
Kacper <kacper@kacper.(none)>
2012-01-24 18:09:53 +0100

    Dodaje RegExpParsera na probe

---
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RegexpParser.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.daut.re;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ *
+ * @author kacper
+ */
+public class RegexpParser {
+
+    RegexpOperatorTree parse(String regexp, RegexpOperatorManager manager) {
+
+        StringBuilder stringBuilder = new StringBuilder(regexp);
+        String tmp = stringBuilder.toString();
+        ArrayList<RegexpOperator> regexpOperators = new ArrayList<RegexpOperator>();
+        Stack<Moja> regexpOperatorsStack = new Stack<Moja>();
+        List<String> Ids = null;
+        String actualId = null;
+
+        while (tmp.isEmpty()) {
+            Ids = manager.getOperatorsForStringPrefix(tmp);
+            if (Ids.size() == 1) {
+
+                actualId = Ids.get(0);
+                int priority = manager.getPriority(actualId);
+                List<String> separators = manager.getSeparators(actualId);
+                RegexpOperator ro = manager.getFactory(actualId).createOperator(new LinkedList<String>());
+                Moja moja = new Moja(priority, ro);
+
+                if (regexpOperatorsStack.isEmpty() || regexpOperatorsStack.peek().priority < moja.priority) {
+                    regexpOperatorsStack.push(moja);
+                } else if (regexpOperatorsStack.peek().priority >= moja.priority) {
+                    Moja tmpMoja = regexpOperatorsStack.pop();
+                    regexpOperators.add(tmpMoja.regExpOper);
+
+                }
+            }
+        }
+        while (!regexpOperatorsStack.isEmpty()) {
+            regexpOperators.add(regexpOperatorsStack.pop().regExpOper);
+        }
+
+        Stack<RegexpOperatorTree> stack = new Stack<RegexpOperatorTree>();
+        for (RegexpOperator ro : regexpOperators) {
+            if (ro.arity() == 0) {
+                stack.push(new RegexpOperatorTree(ro, new LinkedList<RegexpOperatorTree>()));
+            } else {
+                LinkedList<RegexpOperatorTree> ll = new LinkedList<RegexpOperatorTree>();
+                for (int i = 0; i < ro.arity(); ++i) {
+                    ll.add(stack.pop());
+                }
+                stack.push(new RegexpOperatorTree(ro, ll));
+            }
+        }
+        
+        return stack.pop();
+
+        /*RegexpOperatorTree rot1 = null, rot2 = null;
+        for (RegexpOperator ro : regexpOperators) {
+        if (ro.arity() == 0) {
+        if (rot1 == null) {
+        rot1 = new RegexpOperatorTree(ro, new LinkedList<RegexpOperatorTree>());
+        } else {
+        rot2 = new RegexpOperatorTree(ro, new LinkedList<RegexpOperatorTree>());
+        }
+        } else if (ro.arity() == 1) {
+        if (!(rot2 == null)) {
+        LinkedList<RegexpOperatorTree> ll = new LinkedList<RegexpOperatorTree>();
+        ll.add(rot2);
+        rot2 = new RegexpOperatorTree(ro, ll);
+        } else {
+        LinkedList<RegexpOperatorTree> ll = new LinkedList<RegexpOperatorTree>();
+        ll.add(rot1);
+        rot1 = new RegexpOperatorTree(ro, ll);
+        }
+        } else if (ro.arity() == 2) {
+        LinkedList<RegexpOperatorTree> ll = new LinkedList<RegexpOperatorTree>();
+        ll.add(rot1);
+        ll.add(rot2);
+        rot1 = new RegexpOperatorTree(ro, ll);
+        rot1 = null;
+        rot2 = null;
+        }
+        }*/
+
+        //return null;
+    }
+
+    class Moja {
+
+        public Moja(int priority, RegexpOperator regExpOper) {
+            this.priority = priority;
+            this.regExpOper = regExpOper;
+        }
+        int priority;
+        RegexpOperator regExpOper;
+    ;
+}
+}
